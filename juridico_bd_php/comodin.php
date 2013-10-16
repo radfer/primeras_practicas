ebe6cd1594b2ab866adb7cda2381b92fe6b7fbcc
mppfiles <mppfiles@gmail.com>
2013-10-15 21:08:10 -0300

    corrijo typos

--- juridico_bd_php/comodin.php
+++ juridico_bd_php/comodin.php
@@ -1,6 +1,4 @@
 <?php 
 
-		ini_set('error_reportin', E_ALL);//Notifica de todos los errores de php
-		header('Content-Type: text/html; charset=UTF-8');//Que el navegador interprete la salida del script como uft-8
-
-?>
+		ini_set('error_reporting', E_ALL);//Notifica de todos los errores de php
+		header('Content-Type: text/html; charset=UTF-8');//Que el navegador interprete la salida del script como utf-8

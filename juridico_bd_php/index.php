c0b505e60b500e2e7e2526a69495934b93b0d04b
universidad <maruchiin@yahoo.com.ar>
2013-10-25 10:37:29 -0300

    pequeños cambios

--- juridico_bd_php/index.php
+++ juridico_bd_php/index.php
@@ -1,3 +1,3 @@
 <?php 
-	header('Location: ../view/vista1.php');
+	header('Location: controler/control.php');
 ?>
--- juridico_bd_php/model/conexion/ObjetoConexion.php
+++ juridico_bd_php/model/conexion/ObjetoConexion.php
@@ -7,7 +7,7 @@
 		{
 			try
 			{
-				require dirname(__FILE__).'./password.php';//archivo donde se encuentran los usuarios y las claves de la BD
+				require dirname(__FILE__).'/password.php';//archivo donde se encuentran los usuarios y las claves de la BD
 				$this->conexion = new PDO($host, $user, $password1, array(PDO::ATTR_EMULATE_PREPARES=>false, PDO::ATTR_ERRMODE
 				=>PDO::ERRMODE_EXCEPTION));//el último arreglo se pone para que el prepared statement lance
 										//el error, ya que por defecto el lenguaje los ignora.
